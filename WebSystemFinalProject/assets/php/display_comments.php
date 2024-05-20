03b82ec66b795954e122cdac34286b6b70390e8f
Arjay Lagunoy <arjaylascano.lagunoy@bicol-u.edu.ph>
2024-05-20 22:04:21 +0800

    database not yet finish

---
+++ WebSystemFinalProject/assets/php/display_comments.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "root";  
+$password = "";     
+$dbname = "comment_db"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT name, comment FROM comments";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<p><strong>" . $row["name"]. "</strong>: " . $row["comment"]. "</p>";
+    }
+} else {
+    echo "No comments yet!";
+}
+
+$conn->close();
+?>
---
+++ WebSystemFinalProject/assets/php/submit_comment.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";  
+$password = "";     
+$dbname = "comment_db"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $conn->real_escape_string($_POST['name']);
+    $email = $conn->real_escape_string($_POST['email']);
+    $comment = $conn->real_escape_string($_POST['comment']);
+
+    $sql = "INSERT INTO comments (name, email, comment) VALUES ('$name', '$email', '$comment')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New comment created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
